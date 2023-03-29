5369b3c9d95176de6f54eb58ec15676587a7a3c3
Linden Burack <linden.burack@gmail.com>
2023-03-29 12:13:10 -0400

    Digestions.bash for Unindexed Bam Files
    Previously, refs were filtered using samtools which required an indexed bam file. For indexing, a bam file must be pre-sorted by ref/index, a very expensive process for large bam files. Most 4DN downloads are ordered according to the fastq input.

--- digestions.bash
+++ digestions.bash
@@ -3,12 +3,8 @@
 
 # For now only sites at a read end on the plus strand are counted
 
-# Filter only for provided ref on plus strand
-PLUS=$(samtools view -h -F 16 $1 $2)
-
 # Filter plus entries for those whose cigar string starts with a match
 # with a digestion overhang at the read end
-PLUS_FILTERED=$(echo "$PLUS" | awk '$6~/^[0-9]+M/ && $10~/^'$3'/ {print $4}')
+PLUS_FILTERED=$(samtools view -h -F 16 $1 | awk '$3~/'$2'/ && $6~/^[0-9]+M/ && $10~/^'$3'/ {print $4}')
 
 echo "$PLUS_FILTERED"
-echo "$MINUS_FILTERED"
